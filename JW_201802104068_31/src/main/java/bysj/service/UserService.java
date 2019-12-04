571b42f5fb5d897dd5d16a8f386a87777c879367
gaoruichao <hy321360661@163.com>
2019-12-04 14:40:46 +0800

    Initial commit

---
+++ JW_201802104068_31/src/main/java/bysj/service/UserService.java
@@ -0,0 +1,39 @@
+package bysj.service;
+
+
+import bysj.dao.UserDao;
+import bysj.domain.User;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+public final class UserService {
+	private UserDao userDao = UserDao.getInstance();
+	private static UserService userService = new UserService();
+	
+	public UserService() {
+	}
+	
+	public static UserService getInstance(){
+		return UserService.userService;
+	}
+
+	public Collection<User> getUsers(){
+		return userDao.findAll();
+	}
+	
+	public User getUser(Integer id) throws SQLException {
+		return userDao.find(id);
+	}
+
+	public User findByUsername(String username) throws SQLException{
+	    return UserDao.getInstance().findByUsername(username);
+    }
+	
+	public User login(String username, String password) throws SQLException{
+		return UserDao.getInstance().login(username, password);
+	}
+	public boolean changePassword(User user) throws SQLException{
+		return UserDao.getInstance().changePassword(user);
+	}
+}
